a8b45b55948d96d1ddd3a5d805d62254aadf7ac6
Azamat Omuraliev <azamat.omu@gmail.com>
2020-04-20 15:46:39 +0200

    move documentation

--- controllable_abstractive_summarization/setup_env_conda.sh
+++
@@ -1,6 +0,0 @@
-conda create -n venv_train python=3.6.5 -y
-source activate venv_train
-conda install pytorch==1.1.0 cudatoolkit=9.0 -c pytorch
-pip install -r requirements.txt
-python3 -m spacy download en_core_web_sm
-
