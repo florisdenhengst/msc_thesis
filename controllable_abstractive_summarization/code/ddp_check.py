a5171e0a6af6398b1f48a9a16773c082567dc14e
Азамат Омуралиев <f00l@vpn-byods-145-109-54-172.vpn.uva.nl>
2020-06-19 14:03:05 +0200

    fix

--- controllable_abstractive_summarization/code/ddp_check.py
+++ controllable_abstractive_summarization/code/ddp_check.py
@@ -1,3 +1,5 @@
+import argparse
+
 import time
 import torch
 import torch.distributed as dist
@@ -6,18 +8,14 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.nn.parallel import DistributedDataParallel as DDP
 
-def example():#rank, world_size):
-    if True:
-        torch.cuda.set_device(0)
-        dist.init_process_group(backend='gloo',
-                                             init_method='env://')
-        print(torch.distributed.get_world_size())
-        print(torch.cuda.device_count())
+parser.add_argument('--distributed', action='store_true', help='enables distributed processes')
+parser.add_argument('--local_rank', default=0, type=int, help='number of distributed processes')
+parser.add_argument('--dist_backend', default='gloo', type=str, help='distributed backend')
 
-    # create default process group
-    # dist.init_process_group("gloo", rank=rank, world_size=world_size)
+
+def example(rank):#rank, world_size):
     # create local model
-    model = nn.Linear(1000, 10).to(0)
+    model = nn.Linear(1000, 10).to(rank)
     # construct DDP model
     ddp_model = DDP(model, device_ids=[rank])
     # define loss function and optimizer
@@ -25,8 +23,8 @@ def example():#rank, world_size):
     optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
     for j in range(100):
         # forward pass
-        outputs = ddp_model(torch.randn(20, 1000).to(0))
-        labels = torch.randn(20, 10).to(0)
+        outputs = ddp_model(torch.randn(20, 1000).to(rank))
+        labels = torch.randn(20, 10).to(rank)
         # backward pass
         loss_fn(outputs, labels).backward()
         # update parameters
@@ -34,7 +32,7 @@ def example():#rank, world_size):
         optimizer.zero_grad()
 
 def main():
-    example()
+    example(dist.get_rank())
     # world_size = 3
     # mp.spawn(example,
     #     args=(world_size,),
@@ -42,6 +40,14 @@ def main():
     #     join=True)
 
 if __name__=="__main__":
+    args = parser.parse_args()
+    if args.distributed:
+        dist.init_process_group(backend=args.dist_backend, init_method='env://')
+        print("Initialized Rank:", dist.get_rank())
+        print(torch.distributed.get_world_size())
+        print(torch.cuda.device_count())
+
+
     duration = []
     for i in range(10):
         start = time.time()
@@ -50,3 +56,6 @@ if __name__=="__main__":
         duration.append(end-start)
     print(f'Runtimes: {duration}')
     print(f'Average run took {sum(duration) / len(duration)}')
+
+
+
