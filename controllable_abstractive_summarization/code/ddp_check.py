be34bea8250cad2cf1c123de79aac411bed45f87
Azamat Omuraliev <aomurali@fs4.cm.cluster>
2020-06-19 13:54:40 +0200

    fix

--- controllable_abstractive_summarization/code/ddp_check.py
+++ controllable_abstractive_summarization/code/ddp_check.py
@@ -7,16 +7,17 @@ import torch.optim as optim
 from torch.nn.parallel import DistributedDataParallel as DDP
 
 def example():#rank, world_size):
-    if args.distributed:
+    if True:
         torch.cuda.set_device(0)
         dist.init_process_group(backend='gloo',
                                              init_method='env://')
         print(torch.distributed.get_world_size())
+        print(torch.cuda.device_count())
 
     # create default process group
     # dist.init_process_group("gloo", rank=rank, world_size=world_size)
     # create local model
-    model = nn.Linear(1000, 10).to(rank)
+    model = nn.Linear(1000, 10).to(0)
     # construct DDP model
     ddp_model = DDP(model, device_ids=[rank])
     # define loss function and optimizer
@@ -24,8 +25,8 @@ def example():#rank, world_size):
     optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
     for j in range(100):
         # forward pass
-        outputs = ddp_model(torch.randn(20, 1000).to(rank))
-        labels = torch.randn(20, 10).to(rank)
+        outputs = ddp_model(torch.randn(20, 1000).to(0))
+        labels = torch.randn(20, 10).to(0)
         # backward pass
         loss_fn(outputs, labels).backward()
         # update parameters
