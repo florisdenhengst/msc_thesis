4483406bed22280499ee54aacea502b9bbead454
Азамат Омуралиев <f00l@wcw-stud-145-109-10-31.wireless.uva.nl>
2020-07-15 15:51:51 +0200

    typo

--- controllable_abstractive_summarization/code/train.py
+++ controllable_abstractive_summarization/code/train.py
@@ -879,7 +879,7 @@ def train():
                     for i, lt in enumerate(control_tokens):
                         logger.info(f'Control category {lt}, output: {outputs[2][i][0]}')
                     # logger.info(f'Batch control performance: {batch_control_performance}')
-                    logger.info(f'Control performance so far: {[sum(p) / len(p) for p in control_results}]')
+                    logger.info(f'Control performance so far: {[sum(p) / len(p) for p in control_results}]}')
 
                     logger.info(f'ROUGE so far: {{key: {metric: float(test_rouge[key][metric]/batch_count) for metric in test_rouge[key].keys()} for key in test_rouge.keys()}}.')
 
